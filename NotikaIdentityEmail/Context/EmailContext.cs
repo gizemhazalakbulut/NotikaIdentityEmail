182d2e68d0ca3a4b6aea276e02776b069caa63a6
gizemhazalakbulut <gizemhazalakbulut123@gmail.com>
2026-09-11 16:57:06 +0300

    Add project files.

---
+++ NotikaIdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotikaIdentityEmail.Context
+{
+    public class EmailContext:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; initial Catalog=NotikaEmailDb; integrated security=true; trust server certificate=true;");
+        }
+    }
+}
